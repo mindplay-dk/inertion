818cea9821afc48c1c40d7fa60c9ee92341504c6
Rasmus Schultz <rasmus@mindplay.dk>
2022-02-20 12:14:41 +0100

    add todos

--- src/reporting.ts
+++ src/reporting.ts
@@ -11,6 +11,21 @@ export function isSuccess(results: Result[]): boolean {
 }
 
 export function report(results: Result[], console: Console): void {
+  // TODO add proper reporting
+  //
+  //      - pick a formatter:
+  //        https://github.com/facebook/jest/tree/main/packages/pretty-format
+  //        https://github.com/hildjj/node-inspect-extracted
+  //
+  //      - or fork this one?
+  //        https://github.com/commenthol/serialize-to-js
+  //
+  //      - add diffing:
+  //        https://www.npmjs.com/package/diff
+  //        
+  //        see note here about picking between character/line diff:
+  //        https://github.com/lorenzofox3/zora/issues/127
+
   console.log(`${results.length} tests completed\n`);
 
   for (const result of results) {

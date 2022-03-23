0134f1a25cfb7de056096d12e27315aaf1d68ead
Rasmus Schultz <rasmus@mindplay.dk>
2022-03-23 13:52:08 +0100

    forget packaging the .d.ts files for now - it's a lost cause with no simple or elegant solution. shipping multiple .d.ts files is unfancy, but it'll work.

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,18 @@
 import { nodeResolve as resolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
 import commonjs from "@rollup/plugin-commonjs";
 
-export default [
-  {
-    input: "src/index.ts",
-    output: {
-      file: "dist/index.js",
-      format: "es",
-      sourcemap: true,
-    },
-    external: ["util"],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })],
+export default {
+  input: "src/index.ts",
+  output: {
+    file: "dist/index.js",
+    format: "es",
+    sourcemap: true,
   },
-  // {
-  //   input: "build/compiled/index.d.ts",
-  //   output: {
-  //     file: "lockstep-api.d.ts",
-  //     format: "es",
-  //   },
-  //   plugins: [dts()],
-  // },
-];
+  external: ["util"],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
+  ],
+};
